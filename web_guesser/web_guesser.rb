7a3cedcaed35405062c4ee89e0de5896edbf302f
flannerychristopher <flannerychristopher@gmail.com>
2017-05-24 14:15:45 +1000

    add color features and guess limit

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 NUMBER = rand(100) + 1
-
+@@limit = 6
 
 def check_guess(dif)
   if dif.between?(1,5)
@@ -16,7 +16,14 @@ def check_guess(dif)
   else
     message = "Correct! You got it right! The secret number is #{NUMBER}"
   end
+end
 
+def change_color(message)
+  case message
+  when "a little too high.", "a little too low." then '#ff9999'
+	when "way too high!", "way too low!" then '#ff3333'
+	else "#33cc33"
+	end
 end
 
 get '/' do
@@ -24,6 +31,18 @@ get '/' do
   guess = params['guess']
   dif = guess.to_i - NUMBER
   message = check_guess(dif)
-  erb :index, :locals => {:number => NUMBER, :message => message}
+  background = change_color(message)
+  @@limit -= 1
+
+  if @@limit == 0 && message != "Correct! You got it right! The secret number is #{NUMBER}"
+    message = "Sorry you ran out of guesses. There is a new number"
+    @@limit = 5
+    NUMBER = rand(100) + 1
+  elsif message == "Correct! You got it right! The secret number is #{NUMBER}"
+    @@limit = 5
+    NUMBER = rand(100) + 1
+  end
+
+  erb :index, :locals => {:number => NUMBER, :message => message, :background => background}
 
 end
